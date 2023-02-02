922eed0d0ebde764523d99ca8143be8c34d070fa
Thulani Mabaso <Thulanitnm@gmail.com>
2023-02-02 17:21:12 +0200

    Update migration history

---
+++ AdaptItAcademy/AdaptITAcademy.DataAccess/Migrations/20230202151923_addressTypesChange.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdaptITAcademy.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addressTypesChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "PostalAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "PhysicalAddresses");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCodePostalAddress",
+                table: "PostalAddresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCodePhysicalAddress",
+                table: "PhysicalAddresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostalCodePostalAddress",
+                table: "PostalAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCodePhysicalAddress",
+                table: "PhysicalAddresses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostalCode",
+                table: "PostalAddresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostalCode",
+                table: "PhysicalAddresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
